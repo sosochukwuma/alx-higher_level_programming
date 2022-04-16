6cb8fb6fa1ca5f349144200813739ef32cff23ea
sosochukwuma <bossan.sb@gmail.com>
2022-04-16 06:21:42 +0100

    python object relational mapping

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,44 +1,24 @@
 #!/usr/bin/python3
-"""list all State objects containing letter a"""
+''' Write a script that lists all State objects
+that contains the letter a from the database '''
 
 from sys import argv
-from model_state import Base, State
-from sqlalchemy import (create_engine)
+from requests import Session
+from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
+from model_state import State, Base
 
 if __name__ == "__main__":
-    engine = create_engine(
-                'mysql+mysqldb://{}:{}@localhost:3306/{}'
-                .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-
     Session = sessionmaker(bind=engine)
     session = Session()
-    for state in session.query(State).\
-            filter(State.name.like('%a%')).order_by(State.id).all():
-        print("{}: {}".format(state.id, state.name))
+    rows = session.query(State.id, State.name).from_statement(text(
+        "SELECT id, name "
+        "FROM states "
+        "WHERE name REGEXP '^.*a.*' "
+    )).all()
+    for id, name in rows:
+        print("{:d}: {:s}".format(id, name))
     session.close()
-
-
-"""
-lists all State objects that contain the
-letter a from the database hbtn_0e_6_usa
-"""
-
-from sys import argv
-from model_state import Base, State
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(argv[1], argv[2], argv[3]))
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    query = session.query(State).filter(State.name.like('%a%')).order_by(
-            State.id)
-
-    for state in query:
-        print("{}: {}".format(state.id, state.name))
